fff376566de7160263a252275ff72264845af316
Shade-Zepheri <ziroalpha@gmail.com>
2020-07-21 22:54:22 -0500

    Initial Commit

---
+++ Shared/ClimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaApp.swift
+//  Shared
+//
+//  Created by Alfonso Gonzalez on 7/21/20.
+//
+
+import SwiftUI
+
+@main
+struct ClimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
